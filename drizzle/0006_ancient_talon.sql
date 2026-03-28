92561272eafefe13f99cb9ba88c83b5cc69bc183
Afzal14786 <mdafzal14786@gmail.com>
2026-03-28 14:00:40 +0530

    fix: added new actorId into database

---
+++ drizzle/0006_ancient_talon.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "notifications" ADD COLUMN "actor_id" uuid;--> statement-breakpoint
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_actor_id_users_id_fk" FOREIGN KEY ("actor_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
