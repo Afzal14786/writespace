0c8c1154b72e9eb1db1628a8dbe038ac40d44ced
Afzal14786 <mdafzal14786@gmail.com>
2026-03-30 14:56:35 +0530

    fix auth.service.ts and ci.yml

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -24,7 +24,7 @@ const RESET_TOKEN_EXPIRE_SEC = 60 * 60;
 const SALT_ROUNDS = 12;
 
 function toPublicUser(user: typeof users.$inferSelect): PublicUser {
-  const {...publicFields } = user;
+  const { passwordHash, googleAuth, githubAuth, ...publicFields } = user;
   return publicFields as unknown as PublicUser;
 }
 
